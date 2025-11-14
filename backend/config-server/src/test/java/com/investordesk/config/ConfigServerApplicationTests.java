dad2ffd6908c75c5f29f21a825c7d2a3b6fa2be9
haalqershi <alqershi.hesham@gmail.com>
2025-11-13 21:42:26 -0600

    feat: Add Config Server and its local config-repo

---
+++ backend/config-server/src/test/java/com/investordesk/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investordesk.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
